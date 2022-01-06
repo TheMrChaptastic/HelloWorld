c79c22d0a6705287ad21d7489a26de18a7e291a3
TheMrChaptastic <chappy@themrchaptastic.com>
2022-01-06 14:35:15 -0600

    Uploaded

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Finally! It's not .Net 6.0!");
+        }
+    }
+}
